19f4f9050016353e6b084a971c5c9a19960d780a
Dave Cecere <davedw@gmail.com>
2012-08-14 11:05:34 -0400

    refactor: split dataflowprocessor interface from spine interface. missed files

---
+++ src/main/java/com/cecere/forkupine/data/storage/ParameterStorage.java
@@ -0,0 +1,31 @@
+/*
+    Copyright 2012 David Cecere (davedw@gmail.com)
+
+    This file is part of Forkupine.
+
+    Forkupine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Forkupine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Forkupine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.cecere.forkupine.data.storage;
+
+import com.cecere.forkupine.data.Data;
+import com.cecere.forkupine.process.parameter.Spine2Parameter;
+
+/**
+ * @author dave
+ *
+ */
+public interface ParameterStorage {
+	public Spine2Parameter<? extends Data> getParameter(String processId, String paramId);
+	public void putParameter(String processId, String paramId, Spine2Parameter<? extends Data> param);
+}
